3179fa7bef76425772dad31667d05f73f06baf80
Home <ninuganti15@gmail.com>
2020-04-30 19:28:16 -0700

    Fitler instances by project. Also, stop and start instances.

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -4,10 +4,24 @@ import click
 session = boto3.Session(profile_name='shotty')
 ec2 = session.resource('ec2')
 
-@click.command()
-def list_instances():
+@click.group()
+def instances():
+    """Comands for instances"""
+
+@instances.command("list")
+@click.option("--project", default=None,
+    help = "only instances for project (tag Project:<name>)")
+def list_instances(project):
     "List EC2 instances"
-    for i in ec2.instances.all():
+    instances = []
+
+    if project:
+        filters = [{"Name":"tag:Project", "Values":[project]}]
+        instances = ec2.instances.filter(Filters=filters)
+    else:
+        instances = ec2.instances.all()
+
+    for i in instances:
         print(', '.join((
         i.id,
         i.instance_type,
@@ -17,5 +31,25 @@ def list_instances():
 
     return
 
+@instances.command("stop")
+@click.option("--project", default=None,
+    help="Only instaces for project")
+def stop_instances(project):
+    "stop Ec2 instances"
+
+    instances = []
+
+    if project:
+        filters = [{"Name":"tag:Project", "Values":[project]}]
+        instances = ec2.instances.filter(Filters=filters)
+    else:
+        instances = ec2.instances.all()
+
+    for i in instances:
+        print("Stopping {0}...". format(i.id))
+        i.stop()
+
+    return
+
 if __name__ == '__main__':
-    list_instances()
+    instances()
